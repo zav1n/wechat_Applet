eb26dee80f5e14a71946f055cee4ae680709ebb6
zaven2019 <1114370611@qq.com>
2020-05-19 00:01:29 +0800

    5-18

--- wechatDemo/pages/test/test.js
+++ wechatDemo/pages/test/test.js
@@ -5,9 +5,82 @@ Page({
    * 页面的初始数据
    */
   data: {
+    array: [{
+      mode: 'scaleToFill',
+      text: 'scaleToFill：不保持纵横比缩放图片，使图片完全适应'
+    }, {
+      mode: 'aspectFit',
+      text: 'aspectFit：保持纵横比缩放图片，使图片的长边能完全显示出来'
+    }, {
+      mode: 'aspectFill',
+      text: 'aspectFill：保持纵横比缩放图片，只保证图片的短边能完全显示出来'
+    }, {
+      mode: 'top',
+      text: 'top：不缩放图片，只显示图片的顶部区域'
+    }, {
+      mode: 'bottom',
+      text: 'bottom：不缩放图片，只显示图片的底部区域'
+    }, {
+      mode: 'center',
+      text: 'center：不缩放图片，只显示图片的中间区域'
+    }, {
+      mode: 'left',
+      text: 'left：不缩放图片，只显示图片的左边区域'
+    }, {
+      mode: 'right',
+      text: 'right：不缩放图片，只显示图片的右边边区域'
+    }, {
+      mode: 'top left',
+      text: 'top left：不缩放图片，只显示图片的左上边区域'
+    }, {
+      mode: 'top right',
+      text: 'top right：不缩放图片，只显示图片的右上边区域'
+    }, {
+      mode: 'bottom left',
+      text: 'bottom left：不缩放图片，只显示图片的左下边区域'
+    }, {
+      mode: 'bottom right',
+      text: 'bottom right：不缩放图片，只显示图片的右下边区域'
+    }],
+    src: 'https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg',
 
+
+    //Swiper
+    background:['pic-1','pic-2'],
+    indicatorDots: true,//显示小圆点
+    autoplay: false,//自动切换
+    circular:true,//是否循环轮播
+    indicatorColor:"rgba(0,0,0)",//指示点颜色
+    indicatorActiveColor:'rgba(200,200,200)',//当前选中的指示点颜色
+    vertical: false,//不知道什么用
+    interval: 2000,//切换时间
+    duration: 500,//滑动动画时长
+
+    //rich-text
+    nodes: [{
+      name: 'div',
+      attrs: {
+        class: 'div_class',
+        style: 'line-height: 60px; color: red;'
+      },
+      children: [{
+        type: 'text',
+        text: '点击控制台会输出文字'
+      }]
+    }],
+    //icon
+    iconSize: [20, 30, 40, 50, 60, 70],
+    iconType: [
+      'success', 'success_no_circle', 'info', 'warn', 'waiting', 'cancel', 'download', 'search', 'clear'
+    ],
+    iconColor: [
+      'red', 'orange', 'yellow', 'green', 'rgb(0,255,255)', 'blue', 'purple'
+    ],
   },
-
+  tap() {
+    console.log('你点击了"点击控制台会输出文字"这段文字')
+  },
+  
   /**
    * 生命周期函数--监听页面加载
    */
